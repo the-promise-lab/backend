0444fcea43b4b79df1e2bbef106d452fb51441e7
MHJ <ahchjang@naver.com>
2025-12-04 14:09:41 +0900

    feat: add seed script for endings and update character group schema (#37)
    - Introduced a new seed script (`seed-endings.ts`) to populate ending data from a CSV file, enhancing the database with structured ending information.
    - Updated the Prisma schema to rename `deathEndActId` to `deathEndingIndex` in the `characterGroup` model for better clarity and consistency.
    - Modified related service and DTO files to accommodate the new `deathEndingIndex` field, ensuring proper data handling in the application.

--- src/game/dto/character-group.dto.ts
+++ src/game/dto/character-group.dto.ts
@@ -10,25 +10,39 @@ export class CharacterGroupDto {
   @IsString()
   code: string;
 
-  @ApiProperty({ example: '재난 속에서 서로를 의지하며 살아남아야 하는 헬스 광인들', description: '캐릭터 그룹 이름' })
+  @ApiProperty({
+    example: '재난 속에서 서로를 의지하며 살아남아야 하는 헬스 광인들',
+    description: '캐릭터 그룹 이름',
+  })
   @IsString()
   name: string;
 
-  @ApiProperty({ example: 'duo_hem_bang.png', description: '캐릭터 그룹 선택 이미지' })
+  @ApiProperty({
+    example: 'duo_hem_bang.png',
+    description: '캐릭터 그룹 선택 이미지',
+  })
   @IsString()
   groupSelectImage: string;
 
-  @ApiProperty({ example: 10099, description: '사망 엔딩 ID', nullable: true })
+  @ApiProperty({
+    example: 8,
+    description: '서든데스 엔딩 인덱스',
+    nullable: true,
+  })
   @IsInt()
   @IsOptional()
-  deathEndActId: number | null;
+  deathEndingIndex: number | null;
 
   @ApiProperty({ example: 1, description: '프롤로그 ID', nullable: true })
   @IsInt()
   @IsOptional()
   prologActId: number | null;
 
-  @ApiProperty({ example: '평범한 가족', description: '캐릭터 그룹 설명', nullable: true })
+  @ApiProperty({
+    example: '평범한 가족',
+    description: '캐릭터 그룹 설명',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description: string | null;
